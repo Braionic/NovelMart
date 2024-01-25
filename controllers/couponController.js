4634ae497d827ce2d460aa2c701cef15c22bbc0b
Braionic <trustedward@gmail.com>
2024-01-25 11:08:16 +0100

    i added the delete coupon route

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -4,19 +4,33 @@ const createCoupon = async (req, res) => {
   try {
     const newCoupon = new CouponModel(req.body);
     const saveCoupon = await newCoupon.save();
-    if(saveCoupon){
-        return res.json(saveCoupon)
+    if (saveCoupon) {
+      return res.json(saveCoupon);
     }
   } catch (error) {
     res.json(error.message);
   }
 };
 
-const UpdateCoupon = async (req, res)=> {
-    const updatedCoupon = await CouponModel.findByIdAndUpdate(req.params.id, {title: req.body.title, discount: req.body.discount, date: req.body.date}, {new: true})
-if(updatedCoupon){
-    res.json(updatedCoupon)
-}
-}
+const UpdateCoupon = async (req, res) => {
+  const updatedCoupon = await CouponModel.findByIdAndUpdate(
+    req.params.id,
+    { title: req.body.title, discount: req.body.discount, date: req.body.date },
+    { new: true }
+  );
+  if (updatedCoupon) {
+    res.json(updatedCoupon);
+  }
+};
 
-module.exports = {createCoupon, UpdateCoupon};
+const deleteCoupon = async (req, res) => {
+  try {
+    const deletedCoupon = await CouponModel.findByIdAndDelete(req.body.id);
+    if (deletedCoupon) {
+      res.json(deletedCoupon);
+    }
+  } catch (error) {
+    res.json(error.message);
+  }
+};
+module.exports = { createCoupon, UpdateCoupon, deleteCoupon };
--- routes/couponRouter.js
+++ routes/couponRouter.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const {createCoupon, UpdateCoupon} = require('../controllers/couponController')
+const {createCoupon, UpdateCoupon, deleteCoupon} = require('../controllers/couponController')
 const router = express.Router()
 
 
 router.post('/createCoupon', createCoupon)
 router.post('/updateCoupon/:id', UpdateCoupon)
+router.delete('/deletecoupon', deleteCoupon)
 
 module.exports = router
